b618bb498477d1e7033b6acc4630eef1824e60b9
Orishigami <641463017@crru.ac.th>
2023-11-20 23:31:55 +0700

    t1

---
+++ work/go.mod
@@ -0,0 +1,3 @@
+module github.com/Orishigami/go-network2/work
+
+go 1.21.4
---
+++ work/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Person struct {
+	name   string
+	age    int
+	job    string
+	salary int
+}
+
+func main() {
+	var student1 string = "John" //type is string
+	var student2 = "Jane"        //type is inferred
+	x := 2                       //type is inferred
+
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
+
+	var a string
+	var b int
+	var c bool
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	var d, e, f, g int = 1, 3, 5, 7
+
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+
+	var h bool = true    // Boolean
+	var i int = 5        // Integer
+	var j float32 = 3.14 // Floating point number
+	var k string = "Hi!" // String
+
+	fmt.Println("Boolean: ", h)
+	fmt.Println("Integer: ", i)
+	fmt.Println("Float:   ", j)
+	fmt.Println("String:  ", k)
+
+	var arr1 = [3]int{1, 2, 3}
+	arr2 := [5]int{4, 5, 6, 7, 8}
+
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+
+	arr3 := [6]int{10, 11, 12, 13, 14, 15}
+	myslice := arr3[2:4]
+
+	fmt.Printf("myslice = %v\n", myslice)
+	fmt.Printf("length = %d\n", len(myslice))
+	fmt.Printf("capacity = %d\n", cap(myslice))
+
+	myslice1 := []int{}
+	fmt.Println(len(myslice1))
+	fmt.Println(cap(myslice1))
+	fmt.Println(myslice1)
+
+	myslice2 := []string{"Go", "Slices", "Are", "Powerful"}
+	fmt.Println(len(myslice2))
+	fmt.Println(cap(myslice2))
+	fmt.Println(myslice2[2])
+	fmt.Println(myslice2)
+
+	var l = 15 + 25
+	fmt.Println(l)
+
+	var (
+		sum1 = 100 + 50    // 150 (100 + 50)
+		sum2 = sum1 + 250  // 400 (150 + 250)
+		sum3 = sum2 + sum2 // 800 (400 + 400)
+	)
+	fmt.Println(sum3)
+
+	var m = 10
+	m += 5
+	fmt.Println(m)
+
+	var pers1 Person
+
+	pers1.name = "Hege"
+	pers1.age = 45
+	pers1.job = "Teacher"
+	pers1.salary = 6000
+
+	fmt.Println("Name: ", pers1.name)
+	fmt.Println("Age: ", pers1.age)
+	fmt.Println("Job: ", pers1.job)
+	fmt.Println("Salary: ", pers1.salary)
+
+}
